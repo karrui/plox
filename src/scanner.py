f9a9334d7fee53183b6970a96fd8e509daf53a33
Kar Rui <karrui.lau@gmail.com>
2022-09-23 19:46:43 +0800

    feat: add handler for comments, 'privatise' some functions

--- src/scanner.py
+++ src/scanner.py
@@ -12,66 +12,78 @@ class Scanner:
         self.tokens = []
 
     def scan_tokens(self) -> list[str]:
-        while not self.is_at_end():
+        while not self._is_at_end():
             self.start = self.current
             self.scan_token()
 
         self.tokens.append(Token(T.EOF, "", None, self.line))
         return self.tokens
 
-    def is_at_end(self) -> bool:
+    def _is_at_end(self) -> bool:
         return self.current >= len(self.source)
 
-    def advance(self) -> str:
+    def _advance(self) -> str:
         self.current += 1
         return self.source[self.current - 1]
 
-    def add_token(self, type: T) -> None:
-        self._add_token(type, None)
+    def _add_token(self, type: T) -> None:
+        self._add_token_literal(type, None)
 
-    def _add_token(self, type: T, literal: object) -> None:
+    def _add_token_literal(self, type: T, literal: object) -> None:
         text = self.source[self.start:self.current]
         self.tokens.append(Token(type, text, literal, self.line))
 
-    def match(self, expected: str) -> bool:
-        if self.is_at_end():
+    def _match(self, expected: str) -> bool:
+        if self._is_at_end():
             return False
         if self.source[self.current] != expected:
             return False
         self.current += 1
         return True
 
+    def _peek(self) -> str:
+        if self._is_at_end():
+            return '\0'
+        return self.source[self.current]
+
     def scan_token(self) -> None:
-        c = self.advance()
+        c = self._advance()
         match c:
             case '(':
-                self.add_token(T.LEFT_PAREN)
+                self._add_token(T.LEFT_PAREN)
             case ')':
-                self.add_token(T.RIGHT_PAREN)
+                self._add_token(T.RIGHT_PAREN)
             case '{':
-                self.add_token(T.LEFT_BRACE)
+                self._add_token(T.LEFT_BRACE)
             case '}':
-                self.add_token(T.RIGHT_BRACE)
+                self._add_token(T.RIGHT_BRACE)
             case ',':
-                self.add_token(T.COMMA)
+                self._add_token(T.COMMA)
             case '.':
-                self.add_token(T.DOT)
+                self._add_token(T.DOT)
             case '-':
-                self.add_token(T.MINUS)
+                self._add_token(T.MINUS)
             case '+':
-                self.add_token(T.PLUS)
+                self._add_token(T.PLUS)
             case ';':
-                self.add_token(T.SEMICOLON)
+                self._add_token(T.SEMICOLON)
             case '*':
-                self.add_token(T.STAR)
+                self._add_token(T.STAR)
             case '!':
-                self.add_token(T.BANG_EQUAL if self.match('=') else T.BANG)
+                self._add_token(T.BANG_EQUAL if self._match('=') else T.BANG)
             case '=':
-                self.add_token(T.EQUAL_EQUAL if self.match('=') else T.EQUAL)
+                self._add_token(T.EQUAL_EQUAL if self._match('=') else T.EQUAL)
             case '<':
-                self.add_token(T.LESS_EQUAL if self.match('=') else T.LESS)
+                self._add_token(T.LESS_EQUAL if self._match('=') else T.LESS)
             case '>':
-                self.add_token(
-                    T.GREATER_EQUAL if self.match('=') else T.GREATER)
+                self._add_token(
+                    T.GREATER_EQUAL if self._match('=') else T.GREATER)
+            case '/':
+                if self._match('/'):
+                    # A comment goes until the end of the line
+                    while self._peek() != '\n' and not self._is_at_end():
+                        self._advance()
+                else:
+                    self._add_token(T.SLASH)
             case _:
                 Error.error(self.line, "Unexpected character.")
