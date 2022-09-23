897c1483a9d7db7285767008d30ff0518169392f
Kar Rui <karrui.lau@gmail.com>
2022-09-23 17:08:02 +0800

    feat: add error handling code following book

--- plox/Plox.py
+++ plox/Plox.py
@@ -5,7 +5,8 @@ from scanner import Scanner
 
 class Plox:
     def __init__(self) -> None:
-        print(sys.argv)
+        self.had_error = False
+
         # 1st element of sys.argv is always invoked file
         if len(sys.argv) > 2:
             print('Usage: plox [script]')
@@ -15,11 +16,12 @@ class Plox:
         else:
             self.run_prompt()
 
-    def run_file(self, file_path: str):
+    def run_file(self, file_path: str) -> None:
         with open(file_path) as file:
             file_data = file.read()
             self.run(file_data)
-            # TODO: Handle error
+            if (self.had_error):
+                sys.exit(65)
 
     def run(self, source: str):
         scanner = Scanner(source)
@@ -28,15 +30,23 @@ class Plox:
         for token in tokens:
             print(token)
 
-    def run_prompt(self):
+    def run_prompt(self) -> None:
         while True:
             try:
                 line = input("> ")
                 if line == "":
                     break
                 self.run(line)
+                self.had_error = False  # Reset errors if any in prompt
             except EOFError:
                 break
+
+    def error(self, line: int, message: str) -> None:
+        self.report(line, '', message)
+
+    def report(self, line: int, where: str, message: str) -> None:
+        print(f"[line {line}] Error{where}: {message}")
+        self.had_error = True
 
 
 if __name__ == "__main__":
