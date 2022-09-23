194d30cfd9883ad6e5e2bc208e00bbec36f87aec
Kar Rui <karrui.lau@gmail.com>
2022-09-23 18:14:54 +0800

    feat: implement chapter 4 scanner functions

--- src/scanner.py
+++ src/scanner.py
@@ -1,4 +1,5 @@
 from _token import Token
+from error import Error
 from token_type import TokenType as T
 
 
@@ -21,5 +22,39 @@ class Scanner:
     def is_at_end(self) -> bool:
         return self.current >= len(self.source)
 
-    def scan_token(self):
-        pass
+    def advance(self) -> str:
+        self.current += 1
+        return self.source[self.current - 1]
+
+    def add_token(self, type: T) -> None:
+        self._add_token(type, None)
+
+    def _add_token(self, type: T, literal: object) -> None:
+        text = self.source[self.start:self.current]
+        self.tokens.append(Token(type, text, literal, self.line))
+
+    def scan_token(self) -> None:
+        c = self.advance()
+        match c:
+            case '(':
+                self.add_token(T.LEFT_PAREN)
+            case ')':
+                self.add_token(T.RIGHT_PAREN)
+            case '{':
+                self.add_token(T.LEFT_BRACE)
+            case '}':
+                self.add_token(T.RIGHT_BRACE)
+            case ',':
+                self.add_token(T.COMMA)
+            case '.':
+                self.add_token(T.DOT)
+            case '-':
+                self.add_token(T.MINUS)
+            case '+':
+                self.add_token(T.PLUS)
+            case ';':
+                self.add_token(T.SEMICOLON)
+            case '*':
+                self.add_token(T.STAR)
+            case _:
+                Error.error(self.line, "Unexpected character.")
