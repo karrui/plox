cab483580c127ce324cd4830827dfb62205d0ee2
Kar Rui <karrui.lau@gmail.com>
2022-09-23 19:57:19 +0800

    feat(Scanner): handle string literals

--- src/scanner.py
+++ src/scanner.py
@@ -5,46 +5,60 @@ from token_type import TokenType as T
 
 class Scanner:
     def __init__(self, source: str) -> None:
-        self.start = 0
-        self.current = 0
-        self.line = 1
-        self.source = source
-        self.tokens = []
+        self._start = 0
+        self._current = 0
+        self._line = 1
+        self._source = source
+        self._tokens = []
 
     def scan_tokens(self) -> list[str]:
         while not self._is_at_end():
-            self.start = self.current
+            self._start = self._current
             self.scan_token()
 
-        self.tokens.append(Token(T.EOF, "", None, self.line))
-        return self.tokens
+        self._tokens.append(Token(T.EOF, "", None, self._line))
+        return self._tokens
 
     def _is_at_end(self) -> bool:
-        return self.current >= len(self.source)
+        return self._current >= len(self._source)
 
     def _advance(self) -> str:
-        self.current += 1
-        return self.source[self.current - 1]
+        self._current += 1
+        return self._source[self._current - 1]
 
     def _add_token(self, type: T) -> None:
         self._add_token_literal(type, None)
 
     def _add_token_literal(self, type: T, literal: object) -> None:
-        text = self.source[self.start:self.current]
-        self.tokens.append(Token(type, text, literal, self.line))
+        text = self._source[self._start:self._current]
+        self._tokens.append(Token(type, text, literal, self._line))
 
     def _match(self, expected: str) -> bool:
         if self._is_at_end():
             return False
-        if self.source[self.current] != expected:
+        if self._source[self._current] != expected:
             return False
-        self.current += 1
+        self._current += 1
         return True
 
     def _peek(self) -> str:
         if self._is_at_end():
             return '\0'
-        return self.source[self.current]
+        return self._source[self._current]
+
+    def _string(self) -> None:
+        while self._peek() != '"' and not self._is_at_end():
+            if self._peek() == '\n':
+                self._line += 1
+            self._advance()
+        if self._is_at_end():
+            Error.error(self._line, "Unterminated string")
+            return
+        # The closing `"`.
+        self._advance()
+        # Trim the surrounding quotes.
+        value = self._source[self._start+1:self._current - 1]
+        self._add_token_literal(T.STRING, value)
 
     def scan_token(self) -> None:
         c = self._advance()
@@ -88,6 +102,8 @@ class Scanner:
             case ' ' | '\r' | '\t':
                 pass
             case '\n':
-                self.line += 1
+                self._line += 1
+            case '"':
+                self._string()
             case _:
-                Error.error(self.line, "Unexpected character.")
+                Error.error(self._line, "Unexpected character.")
