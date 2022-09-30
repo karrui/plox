df600ab279307d3b98c75ece5420cae958896d96
Kar Rui <karrui.lau@gmail.com>
2022-09-30 16:11:50 +0800

    feat(Interpreter): add error handling

--- src/interpreter.py
+++ src/interpreter.py
@@ -1,8 +1,23 @@
 from decorators.visitor import visitor
 from expr import Binary, Expr, Grouping, Literal, Unary
+from _token import Token
 from token_type import TokenType as T
 from utils.equality import is_equal
 from utils.truthy import is_truthy
+
+
+class LoxRuntimeError(RuntimeError):
+    def __init__(self, token: Token, message: str) -> None:
+        super().__init__(token, message)
+        self.token = token
+
+
+def check_number_operands(operator: Token, *operands: object):
+    for operand in operands:
+        if not isinstance(operand, float):
+            err_msg = "Operand must be a number" if len(
+                operands) > 1 else "Operands must be numbers"
+            raise LoxRuntimeError(operator, err_msg)
 
 
 class Interpreter:
@@ -37,18 +52,25 @@ class Interpreter:
 
         match (expr.operator.type):
             case T.GREATER:
+                check_number_operands(expr.operator, left, right)
                 return float(left) > float(right)
             case T.GREATER_EQUAL:
+                check_number_operands(expr.operator, left, right)
                 return float(left) >= float(right)
             case T.LESS:
+                check_number_operands(expr.operator, left, right)
                 return float(left) < float(right)
             case T.LESS_EQUAL:
+                check_number_operands(expr.operator, left, right)
                 return float(left) <= float(right)
             case T.MINUS:
+                check_number_operands(expr.operator, left, right)
                 return float(left) - float(right)
             case T.SLASH:
+                check_number_operands(expr.operator, left, right)
                 return float(left) / float(right)
             case T.STAR:
+                check_number_operands(expr.operator, left, right)
                 return float(left) * float(right)
             case T.PLUS:
                 # Only perform actions when both left and right instances are of the same type.
@@ -56,6 +78,8 @@ class Interpreter:
                     return left + right
                 if isinstance(left, str) and isinstance(right, str):
                     return left + right
+                raise LoxRuntimeError(
+                    expr.operator, "Operands must be two numbers or two strings.")
             case T.BANG_EQUAL:
                 return not is_equal(left, right)
             case T.EQUAL_EQUAL:
